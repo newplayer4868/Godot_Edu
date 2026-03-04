4ad306ed46a1374de90026806e0d5d753de59e51
jjwoo321 <jjw4868@gmail.com>
2026-03-05 06:52:38 +0900

    코드 분석, get_axis 사용법과 move towrd의 이해, 마찰력 시연과 이동

--- accident.gd
+++ accident.gd
@@ -1,24 +1,36 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 300.0 #속도
+const JUMP_VELOCITY = -400.0 #이건 점프 할떄 힘줄 방향 위로 -400가속
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#중력 추가
+	if not is_on_floor(): #만약 플로어 위에 있지 않는다며느
+		velocity += get_gravity() * delta # 중력 가속도로 떨어져라 
+		#get 그래비티가 뭔데
+		#Vector2 get_gravity() const
+		#Returns the gravity vector computed from all sources  
+		#that can affect the body, including all gravity 
+		#overrides from Area2D nodes and the global world gravity.
+		#대충 2d 영역의 전세계에 적용될 중력 값을 get해오는 거 같은데 set_gravity 있나? 없는거 가틍ㄴ데
+		
+		
+ 
 
-	# Handle jump.
+	# 눌렸을 떄 작동한다. 스페이스바가 눌렸고, 만약 바닥 위에 있다면,
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY #점프하라 
+		#velocity 이게 약간 해당 방향으로 힘을 부여해서 사출 시키는 느낌인듯?
+		#포물선 그려짐
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction = Input.get_axis("ui_left", "ui_right") #방향은 방향키로 받는다 미리 받을 키 지정한건가 디렉션은 저 키로만 값이 바뀐다?
+	if direction: 
+		velocity.x = direction * SPEED 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
