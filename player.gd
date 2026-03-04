fe8fcab507171164c5a2ee503bebdff5ac3de456
jjwoo321 <jjw4868@gmail.com>
2026-03-05 06:09:58 +0900

    process 사용법과 방향키 지정을 통한 이동 시연

--- player.gd
+++ player.gd
@@ -1,14 +1,17 @@
 extends Node2D
+# 초당 이동 픽셀 수
+# 전역 변수? 같은 느낌으로 선언하고 끌어다 쓰는듯
+var speed = 300 
 
-#이건 단순 한번 실행
+
 func _ready():
-	print("시스템 준비 완료!")
-	
-	# 다른 언어랑 똑같이 $이걸로 개체 선택하는 거임
-	# $개체.개체속성
-	# 이경우는 스프라이트 2d의 자식 label의 속성 text를 수정한것
-	$Sprite2D/Label.text = "Hello, GDScript!"
+	$Sprite2D/Label.text = "시작함을 알리는 텍스트"
 
-#여기는 반복 실행
 func _process(delta):
-	pass
+	# 키가 눌려있다면 좌표 증감
+	#delta를 곱하면 기기 성능 따라 퍼포먼스 차이가 안일어난다고 함
+	
+	if Input.is_action_pressed("ui_right"):position.x += speed * delta
+	if Input.is_action_pressed("ui_left"):position.x -= speed * delta
+	if Input.is_action_pressed("ui_down"):position.y += speed * delta
+	if Input.is_action_pressed("ui_up"):position.y -= speed * delta
